7df5dd936dda15f8f16da5441de612b295f2e2a1
Solveig Bjørkholt <solveig.bjorkholt@gmail.com>
2021-06-06 16:28:21 +0200

    mail

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-$(document).ready(function() {
+// Get the modal
+var modal = document.getElementById("myModal");
 
-    $('.js--scroll-to-about').click(function () {
-        $('html, body').animate({scrollTop: $('.js--about').offset().top}, 1000);
-    });
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+$(document).ready(function () {
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+    modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+    }
 })
-
-window.scrollBy({ 
-    top: 100, // could be negative value
-    left: 0, 
-    behavior: 'smooth' 
-  });
